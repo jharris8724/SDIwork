6b62eac70b08ebf6b6ece6f4873f5adc77ab918a
Jeffrey Harris <jharris8724@fullsail.edu>
2012-03-14 23:16:59 -0700

    Added story and groupMembers function.

--- SDI_Project3/Harris_Jeffrey_assignment3.js
+++ SDI_Project3/Harris_Jeffrey_assignment3.js
@@ -1,1 +1,27 @@
 alert("JavaScript works!");
+
+var say = function (message) {console.log(message); };
+
+var groupMembers = function () {
+	var members = ["Jeff", "Steve"];
+	var addmembers = function (member) {
+		members.push(member);
+	};
+	var memberList = function () {
+		say("Members in group: " + members + ".");
+	};
+	
+	return {
+		"addmembers": addmembers,
+		"memberList": memberList
+		};
+};
+
+var group = groupMembers();
+group.addmembers("Neal");
+group.memberList();
+group.addmembers("Rhett");
+group.memberList();
+
+
+
