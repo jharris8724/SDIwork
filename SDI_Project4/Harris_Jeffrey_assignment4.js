f130ac118de2aa1c0a0773c4e8147d1d27af6bdd
Jeffrey Harris <jharris8724@fullsail.edu>
2012-03-19 19:39:27 -0700

    Added Project 4 files to repo.

---
+++ SDI_Project4/Harris_Jeffrey_assignment4.js
@@ -0,0 +1,96 @@
+alert("JavaScript works!");
+
+
+
+
+var jeffhLib = function () {
+
+	var isPhoneNumber = function (number) {
+	
+		if (number.length === 12) {
+			if (number.charAt(3) && number.charAt(7) === "-") {
+			isPhoneNumber = true;
+			console.log('"'+ number +'"' + " is a valid phone number.");
+			}
+			else {
+			isPhoneNumber = false;
+			console.log('"'+ number +'"' + " is a not valid phone number.");
+			};
+		}
+		else {
+		isPhoneNumber = false;
+		console.log('"'+ number +'"' + " is a not valid phone number.");
+		};
+	}; 
+		
+	
+	
+	
+	return {
+		"isPhoneNumber": isPhoneNumber
+	};
+};
+var lib = jeffhLib();
+
+
+var number = "123-456-7890";
+lib.isPhoneNumber(number);
+
+/*
+var myString = "407-679-0100";
+
+var end = myString.indexOf("-");
+
+console.log(myString.substring(0,end));
+
+var start = myString.indexOf("-")+1;
+var end = myString.lastIndexOf("-");
+
+console.log(myString.substring(start,end));
+
+var start = myString.lastIndexOf("-")+1;
+var end = myString.length;
+
+console.log(myString.substring(start,end));
+
+
+
+
+var isPhoneNumber = function (numberString) {
+	var myString = numberString;
+	
+	if (myString.length === 12)
+	var indexString = function () {
+		
+	};
+	
+	if (myString === ) {};
+	return ;
+};
+
+
+
+
+//var phoneNumber = isPhoneNumber("123-456-7890");
+
+
+var titleCase = function (string) {
+	if (string.length === 12) {
+		var areaCode = string.indexOf(0,4),
+			firstThree = string.indexOf(5,8),
+			lastFour = string.indexOf(9,12)
+		;
+		
+		console.log(string.substring(areaCode));
+		console.log(firstThree);
+		console.log(lastFour);
+		
+	};
+	
+};
+
+
+
+var word = titleCase("hello world!");
+
+*/
