553b945562090789b8be10512af1ee3af9043337
Jeffrey Harris <jharris8724@fullsail.edu>
2012-03-21 22:26:34 -0700

    Added findValue and isURL function. Renamed returnNumber function to convertToNumber. Started code for isEmail and titleCase function.

--- SDI_Project4/Harris_Jeffrey_assignment4.js
+++ SDI_Project4/Harris_Jeffrey_assignment4.js
@@ -27,26 +27,54 @@ var jeffhLib = function () {
 					isPhoneNumber = false;
 					console.log('"' + number + '"' + " is a not valid phone number.");
 				};
-			}; 
+			}; // completed
 			
 			var isEmail = function (email) {
 				
-			};
+			}; // not completed
+			
+			var isURL = function (website) {
+				var someURL = website.substring(0,5),
+					someSecureURL = website.substring(0,6)
+				;
+				if(someURL === "http:" || someSecureURL === "https:") {
+					console.log('"' + website + '"' + " is a URL!");
+				}
+				else {
+				console.log('"' + website + '"' + " is not a URL!");
+				}
+
+			}; // completed
+			
+			var titleCase = function (string) {
+				//string.toLowerCase.replace(/^(.)|\s(.)/g)
+			
+			}; // not completed
 			
 			var addDecimal = function (number) {
 				console.log(number.toFixed(2));
-			};
+			}; // completed
 			
-			var showNumber = function (string) {
+			var convertToNumber = function (string) {
 				console.log(parseFloat(string));
-			};
-	
+			}; // completed
+			
+			var findValue = function (array, givenNumber) {
+				var arrayNum = 0;
+				for (var i = 0; arrayNum <= givenNumber; i++) {
+					arrayNum = array[i]
+				};
+				console.log(arrayNum);
+			}; // completed
 	
 	return {
 		"isPhoneNumber": isPhoneNumber,
 		"isEmail": isEmail,
-		"showNumber": showNumber,
-		"addDecimal": addDecimal
+		"isURL": isURL,
+		"titleCase": titleCase,
+		"convertToNumber": convertToNumber,
+		"addDecimal": addDecimal,
+		"findValue": findValue
 	};
 };
 var lib = jeffhLib();
@@ -58,9 +86,21 @@ lib.isPhoneNumber(phoneNumber);
 var email = "johndoe123@someaccount.com";
 lib.isEmail(email);
 
-var stringNumber = "24"
-lib.showNumber(stringNumber);
+var website = "http://fullsail.edu";
+lib.isURL(website);
+
+var title = "hello world!"
+lib.titleCase(title);
 
 var number = 10.5
 lib.addDecimal(number);
+
+var stringNumber = "24"
+lib.convertToNumber(stringNumber);
+
+var array = [0,1,5,12,26,38,49,63,94,102,150,334],
+	givenNumber = 40;
+lib.findValue(array, givenNumber);
+
+
 
